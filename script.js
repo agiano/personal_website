c47fb20fbb7d16fb472993abe63ed76822cd9a75
Alex Giannopoulos <alex.giannopoulos22@gmail.com>
2024-09-09 18:16:41 -0700

    First Draft

---
+++ script.js
@@ -0,0 +1,5 @@
+const homeBtn = document.getElementById("home")
+const aboutBtn = document.getElementById("about")
+const otherBtn = document.getElementById("other")
+
+homeBtn.addEventListener("click")
